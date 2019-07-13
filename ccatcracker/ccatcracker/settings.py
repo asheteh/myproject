b92387fc19f045799c0f2d1318395450bd1a62cd
Abhijit Shete <abhijit.shete@happiestminds.com>
2019-07-13 10:50:05 +0530

    chages prod

--- ccatcracker/ccatcracker/settings.py
+++ ccatcracker/ccatcracker/settings.py
@@ -135,11 +135,6 @@ STATICFILES_DIRS =[
 # EMAIL CONFIG
 
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='thinkgeek.testing@gmail.com'
-EMAIL_HOST_PASSWORD='9921471309'
-EMAIL_USE_TLS = True
 
 # Messages
 try:

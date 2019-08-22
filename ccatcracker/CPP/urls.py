832f832c6fcfec7d680cd3ec91430d8c2bb3b1ec
Abhijit Shete <abhijit.shete@happiestminds.com>
2019-08-22 20:46:27 +0530

    cpp

--- ccatcracker/CPP/urls.py
+++ ccatcracker/CPP/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     path('class-in-cpp',views.day3,name='class-in-cpp'),
     path('pointers-in-cpp',views.day4,name='pointers-in-cpp'),
     path('array-in-cpp',views.day4_a,name='array-in-cpp'),
-    
+    path('constructor-in-cpp',views.day5,name='constructor-in-cpp'),
+    path('friend-function-in-cpp',views.day6,name='friend-function-in-cpp'),
+    path('static-members-in-cpp',views.day6_a,name='static-members-in-cpp'),
+    path('overloading-in-cpp',views.day7,name='overloading-in-cpp'),
+    path('Inheritance-in-cpp',views.day8,name='Inheritance-in-cpp'),
     #path('payment_test',views.payment_test,name='payment_test'),
-     
+    path('constructor-In-inheritance-cpp',views.day9,name='constructor-In-inheritance-cpp'),
+    path('virtual-class-in-cpp',views.day10,name='virtual-class-in-cpp'),
 ]
--- ccatcracker/CPP/views.py
+++ ccatcracker/CPP/views.py
@@ -19,3 +19,26 @@ def day4(request):
 
 def day4_a(request):
     return render(request,'cpp/array.html')
+
+def day5(request):
+    return render(request,'cpp/constructor.html')
+
+
+def day6(request):
+    return render(request,'cpp/friend.html')
+
+def day6_a(request):
+     return render(request,'cpp/static.html')
+
+def day7(request):
+    return render(request,'cpp/overloading.html')
+
+
+def day8(request):
+    return render(request,'cpp/inheritance.html')
+
+def day9(request):
+    return render(request,'cpp/inheritance-1.html')
+
+def day10(request):
+    return render(request,'cpp/virtual.html')
